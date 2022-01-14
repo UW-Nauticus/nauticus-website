3c155ce1cfe81d15c1021970dc5d988974327031
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-13 19:57:48 -0800

    Basic template and nav set up

---
+++ src/pages/FAQ.jsx
@@ -0,0 +1,38 @@
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+const FAQ = () => {
+	return (
+		<MKBox
+			alignItems="center"
+			minHeight="100vh"
+			sx={{
+				backgroundImage: ({
+					palette: { gradients },
+					functions: { linearGradient, rgba },
+				}) =>
+					`${linearGradient(
+						rgba(gradients.dark.main, 0.5),
+						rgba(gradients.dark.state, 0.5)
+					)}`,
+				top: "50px",
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+			}}
+		>
+			<div style={{ position: "absolute", top: "200px", width: "100%" }}>
+				<div
+					style={{
+						position: "relative",
+						left: "50%",
+						width: "100px",
+					}}
+				>
+					FAQ
+				</div>
+			</div>
+		</MKBox>
+	);
+};
+
+export default FAQ;
