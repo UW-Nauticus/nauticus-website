d74585b0879e702935d1e470bb60e146a823aae1
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-13 19:59:30 -0800

    Starting from the bottom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-import Base from "components/Base.jsx";
-import AppBar from "components/AppBar";
-import { lightTheme, darkTheme } from "theme/Themes";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { useState } from "react";
-
 function App() {
-	const [dark, setDark] = useState(true);
-
-	return (
-		<ThemeProvider theme={createTheme(lightTheme)}>
-			<AppBar dark={dark} setDark={setDark} />
-			<Base />
-		</ThemeProvider>
-	);
+	return <div>Hello World</div>;
 }
 
 export default App;
