f9816211063747e8bd0dd35cae52912c584e6f8f
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-15 20:45:59 -0800

    Basic template and nav set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { useEffect } from "react";
-
 // react-router components
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // @mui material components
 import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 
 import theme from "assets/theme";
+
+import About from "pages/About";
+import Base from "pages/Base";
+import Contact from "pages/Contact";
+import faq from "pages/FAQ";
 import Header from "pages/Header";
+import SignUp from "pages/SignUp";
+import Sponsors from "pages/Sponsors";
 
 // Material Kit 2 React routes
 // import routes from "routes";
@@ -20,10 +24,22 @@ function App() {
 				rel="stylesheet"
 				href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
 				integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
-				crossorigin="anonymous"
-				referrerpolicy="no-referrer"
+				crossOrigin="anonymous"
+				referrerPolicy="no-referrer"
 			/>
-			<Header />
+			<Router>
+				<Routes>
+					<Route path="*" element={<Base Child={Header} />} />
+					<Route path="/about" element={<Base Child={About} />} />
+					<Route path="/contact" element={<Base Child={Contact} />} />
+					<Route path="/faq" element={<Base Child={faq} />} />
+					<Route path="/join" element={<Base Child={SignUp} />} />
+					<Route
+						path="/sponsors"
+						element={<Base Child={Sponsors} />}
+					/>
+				</Routes>
+			</Router>
 		</ThemeProvider>
 	);
 }
