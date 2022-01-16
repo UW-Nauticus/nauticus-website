d93f5a07d27b8a89fb96d692c720a2d04ecc3b55
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-15 20:01:34 -0800

    Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { useEffect } from "react";
+
+// react-router components
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import theme from "assets/theme";
+import Header from "pages/Header";
+
+// Material Kit 2 React routes
+// import routes from "routes";
+
 function App() {
-	return <div>Hello World</div>;
+	return (
+		<ThemeProvider theme={theme}>
+			<link
+				rel="stylesheet"
+				href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+				integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+				crossorigin="anonymous"
+				referrerpolicy="no-referrer"
+			/>
+			<Header />
+		</ThemeProvider>
+	);
 }
 
 export default App;
