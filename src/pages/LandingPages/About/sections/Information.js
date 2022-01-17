6777c16968c3470e9968a50da4977697dec1b8fc
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-16 19:43:34 -0800

    Work on About Page

---
+++ src/pages/LandingPages/About/sections/Information.js
@@ -0,0 +1,35 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+function Information() {
+	return (
+		<MKBox component="section" py={12}>
+			<Container>
+				<Grid container spacing={3} alignItems="center">
+					Stuff about competition
+				</Grid>
+			</Container>
+		</MKBox>
+	);
+}
+
+export default Information;
