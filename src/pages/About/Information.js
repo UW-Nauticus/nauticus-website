7e1a8359d259d43b24df0b940ff5e6e25f00bd68
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2022-01-23 13:24:07 -0330

    Apply eslint standards to template

--- src/pages/About/Information.js
+++ src/pages/About/Information.js
@@ -12,49 +12,43 @@ Coded by www.creative-tim.com
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
+import React from 'react';
 
 // @mui material components
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 // Material Kit 2 React components
-import MKBox from "components/MKBox";
-import MKTypography from "components/MKTypography";
+import MKBox from '../../components/MKBox';
+import MKTypography from '../../components/MKTypography';
 
 function Information() {
-	return (
-		<MKBox component="section" pt={12}>
-			<Container>
-				<Grid container>
-					<Grid item xs={12} md={12}>
-						<MKTypography variant="h3" color="inherit">
-							Competition
-						</MKTypography>
-						<br />
-						<MKTypography variant="body1" color="text">
-							The United Nations proclaimed a Decade of Ocean
-							Science for Sustainable Development (2021-2030) to
-							support efforts to reverse the cycle of decline in
-							ocean health and to gather the global community
-							behind a common goal: creating improved conditions
-							for sustainable use and development of our world
-							ocean.
-						</MKTypography>
-						<br />
-						<MKTypography variant="body1" color="text">
-							The ocean is the largest component of our planet’s
-							ecosystem; it stabilizes climate and supports life
-							on Earth and human well-being. However, the First
-							World Ocean Assessment report released in 2016 found
-							that much of the ocean is now seriously degraded,
-							with changes and losses in the structure, function,
-							and benefits from ocean systems.
-						</MKTypography>
-					</Grid>
-				</Grid>
-			</Container>
-		</MKBox>
-	);
+  return (
+    <MKBox component="section" pt={12}>
+      <Container>
+        <Grid container>
+          <Grid item xs={12} md={12}>
+            <MKTypography variant="h3" color="inherit">
+              Competition
+            </MKTypography>
+            <br />
+            <MKTypography variant="body1" color="text">
+              The United Nations proclaimed a Decade of Ocean Science for Sustainable Development (2021-2030) to support
+              efforts to reverse the cycle of decline in ocean health and to gather the global community behind a common
+              goal: creating improved conditions for sustainable use and development of our world ocean.
+            </MKTypography>
+            <br />
+            <MKTypography variant="body1" color="text">
+              The ocean is the largest component of our planet’s ecosystem; it stabilizes climate and supports life on
+              Earth and human well-being. However, the First World Ocean Assessment report released in 2016 found that
+              much of the ocean is now seriously degraded, with changes and losses in the structure, function, and
+              benefits from ocean systems.
+            </MKTypography>
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
 }
 
 export default Information;
--- src/pages/Newsletter.js/Newsletter.js
+++ src/pages/Newsletter.js/Newsletter.js
@@ -13,59 +13,43 @@ Coded by www.creative-tim.com
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
+import React from 'react';
 
 // @mui material components
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 // Material Kit 2 React components
-import MKBox from "components/MKBox";
-import MKTypography from "components/MKTypography";
-import MKInput from "components/MKInput";
-import MKButton from "components/MKButton";
+import MKBox from '../../components/MKBox';
+import MKTypography from '../../components/MKTypography';
+import MKInput from '../../components/MKInput';
+import MKButton from '../../components/MKButton';
 
 function Newsletter() {
-	return (
-		<MKBox component="section" my={6}>
-			<Container>
-				<Grid
-					container
-					spacing={0}
-					direction="column"
-					alignItems="center"
-					justifyContent="center"
-				>
-					<Grid item>
-						<MKTypography variant="h3">
-							News {"&"} Updates
-						</MKTypography>
-						<MKTypography variant="body2" color="text" mb={3}>
-							Add your email to our mailing list to learn about
-							fun events and team milestones.
-						</MKTypography>
-						<Grid container spacing={1}>
-							<Grid item xs={8}>
-								<MKInput
-									type="email"
-									label="Email Here..."
-									fullWidth
-								/>
-							</Grid>
-							<Grid item xs={4}>
-								<MKButton
-									variant="gradient"
-									color="info"
-									sx={{ height: "100%" }}
-								>
-									Subscribe
-								</MKButton>
-							</Grid>
-						</Grid>
-					</Grid>
-				</Grid>
-			</Container>
-		</MKBox>
-	);
+  return (
+    <MKBox component="section" my={6}>
+      <Container>
+        <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center">
+          <Grid item>
+            <MKTypography variant="h3">News & Updates</MKTypography>
+            <MKTypography variant="body2" color="text" mb={3}>
+              Add your email to our mailing list to learn about fun events and team milestones.
+            </MKTypography>
+            <Grid container spacing={1}>
+              <Grid item xs={8}>
+                <MKInput type="email" label="Email Here..." fullWidth />
+              </Grid>
+              <Grid item xs={4}>
+                <MKButton variant="gradient" color="info" sx={{ height: '100%' }}>
+                  Subscribe
+                </MKButton>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
 }
 
 export default Newsletter;
