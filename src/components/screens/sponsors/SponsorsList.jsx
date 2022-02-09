b319a0276af7b04350a3d84701aa84c6fd51ebcc
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2022-02-08 21:54:08 -0330

    Add links to sponsor blocks

--- src/components/screens/sponsors/SponsorsList.jsx
+++ src/components/screens/sponsors/SponsorsList.jsx
@@ -1,26 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 import MKBox from '../../common/MKBox';
 import MKTypography from '../../common/MKTypography';
 import sponsorStructure from '../../../utils/sponsorStructure';
 
 function Sponsors() {
-  const sponsors = sponsorStructure.map((sponsor) => (
-    <Grid
-      key={sponsor.name}
-      item
-      xs={12}
-      md={4}
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-      }}
-    >
-      <MKBox component="img" src={sponsor.logo} alt={sponsor.name} width="100%" />
-    </Grid>
-  ));
+  const sponsors = sponsorStructure.map((sponsor) => {
+    // TODO fix this to follow rules of hooks
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [style, setStyle] = useState({ opacity: 0 });
+    return (
+      <Grid
+        key={sponsor.name}
+        item
+        xs={12}
+        md={4}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
+          component="a"
+          href={sponsor.href}
+          target="_blank"
+          rel="noreferrer"
+          borderRadius="0.5rem"
+          p={1}
+          onMouseEnter={() => {
+            setStyle({ opacity: 1 });
+          }}
+          onMouseLeave={() => {
+            setStyle({ opacity: 0 });
+          }}
+          sx={{
+            textAlign: 'center',
+            textDecoration: 'none',
+            transition: 'all 0.2s ease-in-out',
+            '&:hover': {
+              cursor: 'pointer',
+              backgroundColor: ({ palette: { primary } }) => `${primary.main}`,
+            },
+          }}
+        >
+          <Image src={sponsor.logo} alt={sponsor.name} width={1000} height={500} objectFit="contain" />
+          <p
+            style={{
+              color: 'white',
+              transition: 'all 0.2s ease-in-out',
+              ...style,
+            }}
+          >
+            {sponsor.name}
+          </p>
+        </Box>
+      </Grid>
+    );
+  });
 
   return (
     <MKBox component="section" pt={12}>
