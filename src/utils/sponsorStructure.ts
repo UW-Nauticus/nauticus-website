41463196d0613bd556a494cb090f123a4d780034
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2022-02-08 21:54:08 -0330

    Add links to sponsor blocks

--- src/utils/sponsorStructure.ts
+++ src/utils/sponsorStructure.ts
@@ -1,10 +1,23 @@
 interface Sponsor {
   name: string;
   logo: string;
+  href: string;
 }
 
 export default [
-  { name: 'University of Waterloo Faculty of Engineering', logo: '/images/sponsors/waterloo-engineering.png' },
-  { name: 'Waterloo Engineering Endowment Foundation', logo: '/images/sponsors/weef.png' },
-  { name: 'Waterloo Engineering Society', logo: '/images/sponsors/engsoc.png' },
+  {
+    name: 'University of Waterloo Faculty of Engineering',
+    logo: '/images/sponsors/waterloo-engineering.png',
+    href: 'https://uwaterloo.ca/engineering/',
+  },
+  {
+    name: 'Waterloo Engineering Endowment Foundation',
+    logo: '/images/sponsors/weef.png',
+    href: 'https://uwaterloo.ca/engineering-endowment-foundation/',
+  },
+  {
+    name: 'Waterloo Engineering Society',
+    logo: '/images/sponsors/engsoc.png',
+    href: 'https://www.engsoc.uwaterloo.ca/',
+  },
 ] as Array<Sponsor>;
