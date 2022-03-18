917c2fd9433c1fbdbe74a0cdf00ff86e2d9046d7
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2022-03-17 22:38:57 -0230

    Combine software and electrical subteams
    This makes it more clear that the majority of the software requires
    at least some knowledge of electronics for team members to contribute.

--- src/components/screens/team/SubTeams.jsx
+++ src/components/screens/team/SubTeams.jsx
@@ -7,7 +7,6 @@ import MKTypography from '../../common/MKTypography';
 
 // Images
 const mech = '/images/subteams/mech.png';
-const electrical = '/images/subteams/electrical.jpg';
 const software = '/images/subteams/software.png';
 const business = '/images/subteams/business.jpg';
 
@@ -42,43 +41,6 @@ function SubTeams() {
               >
                 <MKBox component="img" src={mech} alt="software" width="75%" />
               </Grid>
-
-              <Grid
-                item
-                xs={12}
-                md={6}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                }}
-              >
-                <MKBox component="img" src={electrical} alt="software" width="100%" />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <MKTypography variant="h4" fontWeight="regular">
-                  Electrical
-                </MKTypography>
-                <br />
-                <MKTypography variant="body2">
-                  The Electrical team is in charge of all custom PCBs and off-the-shelf electronics in the ROV, as well
-                  as the electronics on the surface-side control station. This includes designing and manufacturing
-                  onboard sensors and implementing custom hardware to minimize voltage loss and signal integrity loss
-                  from the surface to the ROV.
-                </MKTypography>
-              </Grid>
-
-              <Grid item xs={12} md={6}>
-                <MKTypography variant="h4" fontWeight="regular">
-                  Software
-                </MKTypography>
-                <br />
-                <MKTypography variant="body2">
-                  The Software team creates the link between the electronics and our pilot. They&apos;re responsible the
-                  GUI for the pilot to use and the communication software for directing commands from the surface-side
-                  control station to the ROV underwater. They&apos;re also the team that creates the embedded software
-                  for the main ROV systems and onboard sensors.
-                </MKTypography>
-              </Grid>
               <Grid
                 item
                 xs={12}
@@ -90,17 +52,18 @@ function SubTeams() {
               >
                 <MKBox component="img" src={software} alt="software" width="100%" />
               </Grid>
-
-              <Grid
-                item
-                xs={12}
-                md={6}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                }}
-              >
-                <MKBox component="img" src={business} alt="software" width="100%" />
+              <Grid item xs={12} md={6}>
+                <MKTypography variant="h4" fontWeight="regular">
+                  Controls
+                </MKTypography>
+                <br />
+                <MKTypography variant="body2">
+                  The Controls team combines electronics and software to provide function to the ROV and establish a
+                  link between the pilot and the ROV. They&apos;re in charge of all custom PCBs and off-the-shelf
+                  electronics in the ROV, as well as the electronics on the surface-side control station. They&apos;re
+                  also responsible for all the software used by the ROV, from the GUI for that the pilot uses to the
+                  embedded software for the main ROV systems and onboard sensors.
+                </MKTypography>
               </Grid>
               <Grid item xs={12} md={6}>
                 <MKTypography variant="h4" fontWeight="regular">
@@ -112,6 +75,17 @@ function SubTeams() {
                   team relies on sponsors to ensure that we have the resources and funding to show our full engineering
                   potential, and the Business team is integral to keeping great sponsor relations.
                 </MKTypography>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                md={6}
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              >
+                <MKBox component="img" src={business} alt="software" width="100%" />
               </Grid>
             </Grid>
           </Grid>
