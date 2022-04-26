06e1335da8c4625424ec692f61851acb8c0437b9
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2022-04-26 01:48:47 -0230

    Fix team images on mobile
    The alternating order of descriptions and images looks good on desktop,
    but it looks bad on mobile. Do not alternate on either platform to
    make them both look good.

--- src/components/screens/team/SubTeams.jsx
+++ src/components/screens/team/SubTeams.jsx
@@ -5,12 +5,66 @@ import Grid from '@mui/material/Grid';
 import MKBox from '../../common/MKBox';
 import MKTypography from '../../common/MKTypography';
 
-// Images
-const mech = '/images/subteams/mech.png';
-const software = '/images/subteams/software.png';
-const business = '/images/subteams/business.jpg';
-
 function SubTeams() {
+  const subTeamList = [
+    {
+      name: 'Mechanical',
+      description:
+        'The Mechanical team is responsible for designing, manufacturing, and validating the frame of the AUV, ' +
+        "the enclosure for the electronics, and the AUV's primary tools. This includes waterproofing, " +
+        'hydrodynamic simulations, and electrical integration.',
+      image: '/images/subteams/mech.png',
+    },
+    {
+      name: 'Controls',
+      description:
+        'The Controls team combines electronics and software to provide function to the AUV. ' +
+        "They're in charge of all custom PCBs and off-the-shelf electronics in the AUV. " +
+        "They're also responsible for all the software used by the AUV, " +
+        'from the onboard computer vision software to the embedded software for the main AUV systems and onboard sensors.',
+      image: '/images/subteams/software.png',
+    },
+    {
+      name: 'Business',
+      description:
+        'The Business team is responsible for fundraising, marketing, and keeping track of team finances. ' +
+        'Our team relies on sponsors to ensure that we have the resources and funding to show our full engineering potential, ' +
+        'and the Business team is integral to keeping great sponsor relations.',
+      image: '/images/subteams/business.jpg',
+    },
+  ];
+
+  const subTeams = subTeamList.map((team) => {
+    const description = (
+      <Grid item xs={12} md={6}>
+        <MKTypography variant="h4" fontWeight="regular">
+          {team.name}
+        </MKTypography>
+        <br />
+        <MKTypography variant="body2">{team.description}</MKTypography>
+      </Grid>
+    );
+    const teamImage = (
+      <Grid
+        item
+        xs={12}
+        md={6}
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <MKBox component="img" src={team.image} alt={team.name} width="100%" />
+      </Grid>
+    );
+
+    return (
+      <>
+        {description} {teamImage}
+      </>
+    );
+  });
+
   return (
     <MKBox component="section" py={12}>
       <Container>
@@ -19,73 +73,7 @@ function SubTeams() {
             <MKTypography variant="h3">Subteams</MKTypography>
             <br />
             <Grid container alignItems="center" spacing={6}>
-              <Grid item xs={12} md={6}>
-                <MKTypography variant="h4" fontWeight="regular">
-                  Mechanical
-                </MKTypography>
-                <br />
-                <MKTypography variant="body2">
-                  The Mechanical team is responsible for designing, manufacturing, and validating the frame of the AUV,
-                  the enclosure for the electronics, and the AUV&apos;s primary tools. This includes waterproofing,
-                  hydrodynamic simulations, and electrical integration.
-                </MKTypography>
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                md={6}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                }}
-              >
-                <MKBox component="img" src={mech} alt="software" width="75%" />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                md={6}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                }}
-              >
-                <MKBox component="img" src={software} alt="software" width="100%" />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <MKTypography variant="h4" fontWeight="regular">
-                  Controls
-                </MKTypography>
-                <br />
-                <MKTypography variant="body2">
-                  The Controls team combines electronics and software to provide function to the AUV. They&apos;re in
-                  charge of all custom PCBs and off-the-shelf electronics in the AUV. They&apos;re also responsible for
-                  all the software used by the AUV, from the onboard computer vision software to the embedded software
-                  for the main AUV systems and onboard sensors.
-                </MKTypography>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <MKTypography variant="h4" fontWeight="regular">
-                  Business
-                </MKTypography>
-                <br />
-                <MKTypography variant="body2">
-                  The Business team is responsible for fundraising, marketing, and keeping track of team finances. Our
-                  team relies on sponsors to ensure that we have the resources and funding to show our full engineering
-                  potential, and the Business team is integral to keeping great sponsor relations.
-                </MKTypography>
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                md={6}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                }}
-              >
-                <MKBox component="img" src={business} alt="software" width="100%" />
-              </Grid>
+              {subTeams}
             </Grid>
           </Grid>
         </Grid>
